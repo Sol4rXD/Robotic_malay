4b912fa441403a05747b9fe3caf5d64e3e0999ce
Filmmm <watcharawish.ons@gmail.com>
2023-10-03 08:36:23 +0700

    Update

--- include/definations.h
+++ include/definations.h
@@ -1,20 +1,26 @@
 #include <Arduino.h>
 
+// Motor right
 #define MOTOR_RIGHT_1 12 // CHECK
 #define MOTOR_RIGHT_2 13 // CHECK
 
+// Motor left
 #define MOTOR_LEFT_1 12  // CHECK
 #define MOTOR_LEFT_2 12  // CHECK
 
+// Motor both PWM
 #define MOTOR_RIGHT_PWM 14  // CHECK
 #define MOTOR_LEFT_PWM 15   // CHECK
 
+// Ultrasonic 1
 #define TRIQ_PIN_LEFT 13 // CHECK
 #define ECHO_PIN_LEFT 13    // CHECK
 
+// Ultrasonic 2
 #define TRIQ_PIN_RIGHT 13 // CHECK
 #define ECHO_PIN_RIGHT 13 // CHECK
 
+// Light sensor
 #define LIGHT_SENSOR_PIN A0 // CHECK
 
 const int TARGET_DISTANCE_THRESHOLD = 20; // CHECK
