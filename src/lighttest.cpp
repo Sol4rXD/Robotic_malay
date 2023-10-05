acc708643b49ee3a739d3acf5e6a705fdebafe1e
Filmmm <watcharawish.ons@gmail.com>
2023-10-05 12:41:08 +0700

    Update code

--- src/lighttest.cpp
+++ src/lighttest.cpp
@@ -1,41 +1,24 @@
 #include <Arduino.h>
 
 unsigned long time;
-int color = 0; // Initialize color as 0, indicating no border detected
-int last = 0;
-int last_color = 0;
-
-void setup() {
-  Serial.begin(9600);
+int color=1,last,last_color;
+void setup()
+{
+ Serial.begin(9600);
 }
-
 void loop() {
-  pinMode(9, OUTPUT);
-  digitalWrite(9, HIGH);
-  delayMicroseconds(12);
-  pinMode(9, INPUT);
-  time = micros();
-  while (digitalRead(9)) {}
-
-  // Adjust this threshold value to match your sensor's behavior
-  int threshold = 1000;
-
-  // Check if a transition from white to black is detected based on the time elapsed
-  if ((micros() - time) > threshold) {
-    color = 1; // Detected the transition from white to black (border)
-  } else {
-    color = 0; // No transition detected (white field)
-  }
-
-  // Print the status of the border
-  if (color != last_color) {
-    if (color == 1) {
-      Serial.println("Approaching the black border");
-    } else {
-      Serial.println("On the white field");
-    }
-  }
-
-  last = micros() - time;
-  last_color = color;
+ pinMode(9,OUTPUT);
+ digitalWrite(9,HIGH);
+ delayMicroseconds(12);
+ pinMode(9,INPUT);
+ time=micros();
+ while(digitalRead(9)) {}
+// Serial.println(micros()-time);
+  
+  if((micros()-time)>last && color==1) color=-1;
+      else if((micros()-time)<last && color==-1) color=1;
+   
+    if(color!=last_color) Serial.print(color),Serial.println(" ");
+    last=micros()-time;
+    last_color=color;
 }
